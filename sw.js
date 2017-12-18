fb91e1c4eafdf1da07257f8df43b7e0d1d919222
NaveenShanmugavel18 <naveensv18@gmail.com>
2017-12-18 16:47:02 +0530

    changed type

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  jsonData = JSON.parse(event.data.text());
+  jsonData = JSON.stringify(event.data.text());
   console.log(jsonData.message);
   const title = 'CreditMantri Test';
   const options = {
