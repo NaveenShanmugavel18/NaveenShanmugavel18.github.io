9012f716d7a5105f891b59c10f15fbf5cbd6d8ba
NaveenShanmugavel18 <naveensv18@gmail.com>
2017-12-18 16:48:25 +0530

    event data log

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ function urlB64ToUint8Array(base64String) {
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
+  console.log(event.data);
   jsonData = JSON.stringify(event.data.text());
   console.log(jsonData.message);
   const title = 'CreditMantri Test';
