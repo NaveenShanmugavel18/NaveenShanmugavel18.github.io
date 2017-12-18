a7c59e30db1f17b3cd52295082cf31e93b819350
NaveenShanmugavel18 <naveensv18@gmail.com>
2017-12-18 16:24:03 +0530

    payload check

--- sw.js
+++ sw.js
@@ -20,13 +20,10 @@ function urlB64ToUint8Array(base64String) {
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-  console.log("Event ",event);
-  console.log("Event data ",event.data);
-  datajson = event.data.json();
 
   const title = 'CreditMantri Test';
   const options = {
-    body: 'Message Received \n ' + datajson.message,
+    body: 'Message Received \n ' + event.data.text().message,
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
